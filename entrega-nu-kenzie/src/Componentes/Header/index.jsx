9f4d6ed46717c0708e2ece86a45649e0310cc26a
Jandesson Moraes <jandessonmoraes@gmail.com>
2023-03-25 00:43:28 -0300

    fist commit

--- entrega-nu-kenzie/src/Componentes/Header/index.jsx
+++ entrega-nu-kenzie/src/Componentes/Header/index.jsx
@@ -1,11 +1,12 @@
 import '../../styles/index.css';
 import '../../styles/pages/style.module.css';
+import logo from '../../assets/logo.svg'
 
 export function Header() {
     return(
         <header>
             <div className="divLogo">
-                <img className='logoKenzie' src='/src/assets/Nu_Kenzie2.png' alt='Logo Kenzie' />
+                <img className='logoKenzie' src={logo} alt='Logo Kenzie' />
             </div>
         </header>
     );
