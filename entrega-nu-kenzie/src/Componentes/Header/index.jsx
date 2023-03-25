6c2f44c078c772fb501fb5007b96b174bd4419b4
Jandesson Moraes <jandessonmoraes@gmail.com>
2023-03-24 23:57:46 -0300

    fist commit

---
+++ entrega-nu-kenzie/src/Componentes/Header/index.jsx
@@ -0,0 +1,12 @@
+import '../../styles/index.css';
+import '../../styles/pages/style.module.css';
+
+export function Header() {
+    return(
+        <header>
+            <div className="divLogo">
+                <img className='logoKenzie' src='/src/assets/Nu_Kenzie2.png' alt='Logo Kenzie' />
+            </div>
+        </header>
+    );
+};
